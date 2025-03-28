991c4d5722e20b7873e827997dfdb92996395223
Dan Jaheny <danjaheny@Dans-MacBook-Pro-2.local>
2025-03-28 11:17:45 +0100

    refactor

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,10 @@
+import { EvmChainId, FordefiProviderConfig } from '@fordefi/web3-provider';
 import { Token } from '@uniswap/sdk-core'
 import { FeeAmount } from '@uniswap/v3-sdk'
+import dotenv from 'dotenv';
+import fs from 'fs'
+
+dotenv.config();
 
 interface ExampleConfig {
   rpc: {
@@ -33,18 +38,30 @@ const USDC_TOKEN = new Token(
   'USD//C'
 )
 
+
+// Configure the Fordefi provider
+export const fordefiConfig: FordefiProviderConfig = {
+  chainId: EvmChainId.NUMBER_1, // Mainnet
+  address: '0x8BFCF9e2764BC84DE4BBd0a0f5AAF19F47027A73', // The Fordefi EVM Vault that will sign the message
+  apiUserToken: process.env.FORDEFI_API_USER_TOKEN ?? (() => { throw new Error('FORDEFI_API_USER_TOKEN is not set'); })(), 
+  apiPayloadSignKey: fs.readFileSync('./fordefi_secret/private.pem', 'utf8') ?? (() => { throw new Error('PEM_PRIVATE_KEY is not set'); })(),
+  rpcUrl: 'https://ethereum-rpc.publicnode.com',
+  skipPrediction: false 
+};
+
+// Configure your Uniswap swap
 export const CurrentConfig: ExampleConfig = {
-    rpc: {
-      local: '',
-      mainnet: 'https://ethereum-rpc.publicnode.com',
-    },
-    tokens: {
-      in: USDC_TOKEN,
-      amountIn: 1, // in natural units
-      out: WETH_TOKEN,
-      poolFee: FeeAmount.MEDIUM
-    },
-    wallet: {
-      address: "0x8BFCF9e2764BC84DE4BBd0a0f5AAF19F47027A73"
-    }
+  rpc: {
+    local: '',
+    mainnet: 'https://ethereum-rpc.publicnode.com',
+  },
+  tokens: {
+    in: USDC_TOKEN,
+    amountIn: 1, // in natural units (1 = 1 whole USDC)
+    out: WETH_TOKEN,
+    poolFee: FeeAmount.MEDIUM
+  },
+  wallet: {
+    address:fordefiConfig.address
   }
+}
